ca136ef28e656942c73a09a68e82d1529beacd11
Ben Reeves <benwolverine2019@gmail.com>
2022-12-14 23:46:30 -0600

    Pointer version.

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,6 @@
 //! # Example
 //!
 //! ```
-//! use std::thread;
 //! use lender_loan::{Lender, Loan};
 //!
 //! fn use_loan(loan: Loan<Vec<i32>>) {
@@ -33,7 +32,7 @@
 //! Lender::with(&value, |lender: &Lender<'_, Vec<i32>>| {
 //!     for _ in 0..100 {
 //!         let loan = lender.lend();
-//!         thread::spawn(move || use_loan(loan));
+//!         std::thread::spawn(move || use_loan(loan));
 //!     }
 //! });
 //!
@@ -42,41 +41,122 @@
 //! ```
 
 #![deny(unsafe_op_in_unsafe_fn)]
-//#![warn(missing_docs)]
+#![warn(missing_docs)]
 
-use core::fmt;
-use std::{hash, ops::Deref, sync::Arc};
+use std::{fmt, hash, marker::PhantomData, ops::Deref, sync::Arc};
 
+/// A scoped reference to a value that can be lent to other threads in a
+/// lifetime-erased way.
+///
+/// This type cannot be constructed manually, you can only get one by calling
+/// [`Lender::with`]. This type also cannot be cloned. It is the "owner" of the
+/// shared reference that is to be lent out.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Lender<'a, T: ?Sized> {
+pub struct Lender<'a, T: 'a + ?Sized> {
+    // Both the lender and all of its loans hold a strong reference to the
     inner: Arc<LoanInner<'a, T>>,
 }
 
-impl<'a, T: ?Sized> Lender<'a, T> {
-    pub fn with<R>(value: &T, op: impl FnOnce(&Lender<'_, T>) -> R) -> R {
+impl<'a, T: 'a + ?Sized> Lender<'a, T> {
+    /// Constructs a [`Lender`] for the provided value.
+    ///
+    /// A reference to the lender is passed to the provided closure.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use lender_loan::{Lender, Loan};
+    ///
+    /// fn use_loan(loan: Loan<Vec<i32>>) {
+    ///     assert_eq!(*loan, vec![1, 2, 3]);
+    /// }
+    ///
+    /// // Create a value that will be lent to other threads.
+    /// let mut value = vec![1, 2, 3];
+    /// Lender::with(&value, |lender: &Lender<'_, Vec<i32>>| {
+    ///     for _ in 0..100 {
+    ///         let loan = lender.lend();
+    ///         std::thread::spawn(move || use_loan(loan));
+    ///     }
+    /// });
+    ///
+    /// // It is safe to modify the value again; `Lender::with` blocks until all loans are dropped.
+    /// value.push(4);
+    /// ```
+    pub fn with<R>(value: &'a T, op: impl FnOnce(&Lender<'a, T>) -> R) -> R {
+        // This one-shot event is borrowed by the `LoanInner` and triggered when
+        // the last `Arc<LoanInner>` is dropped.
         let event = Event::new();
-        let inner = Arc::new(LoanInner {
-            principal: value,
-            event: &event,
-        });
-        let lender = Lender { inner };
+
+        // SAFETY: The event.wait() call below ensures that the event is valid
+        // for at least as long as all `Arc<LoanInner>`s.
+        let inner = unsafe { LoanInner::new(value, &event) };
+
+        let lender = Lender {
+            inner: Arc::new(inner),
+        };
         let result = op(&lender);
 
-        // This is the whole thing that makes it safe.
+        // The lender holds a strong reference to the `LoanInner`. Make sure we
+        // drop it before waiting on the event, otherwise the `LoanInner` will
+        // never drop!
         drop(lender);
+
+        // Wait for all loans to have been dropped.
+        // This is the whole point!!!
         event.wait();
 
         result
     }
 
+    /// Returns the reference to the value being lent.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use lender_loan::Lender;
+    ///
+    /// let value = vec![1, 2, 3];
+    /// Lender::with(&value, |lender| {
+    ///     assert_eq!(lender.get() as *const _, &value as *const _);
+    /// });
+    /// ```
     #[inline]
-    pub fn lend(&self) -> Loan<T>
-    where
-        T: 'static,
-    {
+    pub fn get(&self) -> &'a T {
+        self.inner.principal()
+    }
+
+    /// Returns a lifetime-erased [`Loan`] of the value being lent.
+    ///
+    /// The lender will not go out of scope until all loans have been dropped.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use lender_loan::Lender;
+    ///
+    /// let value = String::from("you owe me tree fiddy");
+    /// Lender::with(&value, |lender| {
+    ///     let loan = lender.lend();
+    ///     std::thread::spawn(move || {
+    ///         println!("{}", loan.as_ref());
+    ///     });
+    /// });
+    #[inline]
+    pub fn lend(&self) -> Loan<T> {
         let loaned_inner: Arc<LoanInner<'a, T>> = self.inner.clone();
 
-        // SAFETY: TODO
+        // Here is where we erase the lifetime.
+        //
+        // SAFETY: There are two things to make sure of:
+        //  1. The `T` pointed to by the `LoanInner` is valid as long as any
+        //     `Loan` exists.
+        //  2. No `&mut T` can be created until all `Loans` are dropped (since
+        //     they are basically `&T`).
+        //
+        // Both of these are achieved by the `event.wait()` call in
+        // `Lender::with`. The event does not fire until all `Loans` have
+        // dropped.
         let loaned_inner: Arc<LoanInner<'static, T>> = unsafe { std::mem::transmute(loaned_inner) };
         Loan {
             inner: loaned_inner,
@@ -87,7 +167,7 @@ impl<'a, T: ?Sized> Lender<'a, T> {
 impl<'a, T: ?Sized> AsRef<T> for Lender<'a, T> {
     #[inline]
     fn as_ref(&self) -> &T {
-        self.inner.principal
+        self.inner.principal()
     }
 }
 
@@ -96,44 +176,131 @@ impl<'a, T: ?Sized> Deref for Lender<'a, T> {
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        self.inner.principal
+        self.inner.principal()
     }
 }
 
+/// A lifetime-erased, reference-counted pointer to a [`Lender`]'s data.
+///
+/// It's basically just an [`Arc`] that tells the [`Lender`] to proceed once all
+/// references are dropped.
+///
+/// This type can only be created by calling [`Lender::lend`].
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Loan<T: 'static + ?Sized> {
+pub struct Loan<T: ?Sized> {
+    // The lifetime is 'static because the lifetime has been erased!
+    // This is okay because `Loan` hands out references with lifetimes no longer
+    // than that of itself, and the stack frame in which the loaned value lives
+    // is guaranteed not to pop until all `Loan`s have dropped.
     inner: Arc<LoanInner<'static, T>>,
 }
 
-impl<T: 'static + ?Sized> AsRef<T> for Loan<T> {
+impl<T: ?Sized> AsRef<T> for Loan<T> {
     #[inline]
     fn as_ref(&self) -> &T {
-        self.inner.principal
+        self.inner.principal()
     }
 }
 
-impl<T: 'static + ?Sized> Deref for Loan<T> {
+impl<T: ?Sized> Deref for Loan<T> {
     type Target = T;
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        self.inner.principal
+        self.inner.principal()
     }
 }
 
-struct LoanInner<'a, T: ?Sized> {
-    /// The thing we are lending.
-    principal: &'a T,
-    /// An event that is signaled when the last reference to the `LoanInner` is dropped.
-    event: &'a Event,
-}
+mod inner {
+    use super::*;
 
-impl<'a, T: ?Sized> Drop for LoanInner<'a, T> {
-    fn drop(&mut self) {
-        self.event.trigger();
+    /// The thing that both the [`Lender`] and [`Loan`] hold strong [`Arc`]
+    /// references to.
+    ///
+    /// It is in a private module so the fields cannot be directly accessed.
+    pub(super) struct LoanInner<'a, T: ?Sized> {
+        /// The thing we are lending. (ha ha, get it, the loan principal?) This
+        /// has the same lifetime 'a as the `Lender` that "owns" the original
+        /// reference.
+        ///
+        /// Note: This would _like_ to be a `&'a T`, but that would require that
+        /// `T: 'static` since `Loan<T>` stores a `LoanInner<'static, T>`. But
+        /// that's an unnecessary restriction; it's okay to lend out loans to
+        /// something like `&'a Foo<'b>` since we know `'b: 'a` is implied by
+        /// the type system.
+        principal: *const T,
+
+        /// An event that is signaled when the last reference to the `LoanInner`
+        /// is dropped. This has a lifetime that is shorter than that of the
+        /// loan principal, but at least as long as this `LoanInner`.
+        event: *const Event,
+
+        _phantom: PhantomData<&'a ()>,
+    }
+
+    impl<'a, T: ?Sized> LoanInner<'a, T> {
+        /// Returns a new [`LoanInner`].
+        ///
+        /// # Safety
+        ///
+        /// The provided [`Event`] reference must be valid for at least as long
+        /// as this [`LoanInner`] is valid, because it is dereferenced in
+        pub(super) unsafe fn new(principal: &'a T, event: &Event) -> Self
+        where
+            T: 'a,
+        {
+            Self {
+                principal: principal as *const T,
+                event: event as *const Event,
+                _phantom: PhantomData,
+            }
+        }
+
+        /// Returns a reference to the value being lent (ha ha, get it, the loan
+        /// principal?).
+        #[inline(always)]
+        pub(super) fn principal(&self) -> &'a T {
+            // SAFETY: The principal pointer was constructed from a `&'a T`.
+            unsafe { &*self.principal }
+        }
+
+        #[inline(always)]
+        fn event(&self) -> &Event {
+            // SAFETY: The event pointer was constructed from a valid `&Event`,
+            // and the caller of new() promised that it would be  valid for at
+            // least as long as this LoanInner.
+            unsafe { &*self.event }
+        }
+    }
+
+    impl<'a, T: ?Sized> Drop for LoanInner<'a, T> {
+        fn drop(&mut self) {
+            self.event().trigger();
+        }
+    }
+
+    /// SAFETY: The pointers in [`LoanInner`] may as well just be shared
+    /// references, so it's `Send` whenever the inner bits are.
+    unsafe impl<'a, T> Send for LoanInner<'a, T>
+    where
+        T: Send + ?Sized,
+        Event: Send,
+    {
+    }
+
+    /// SAFETY: The pointers in [`LoanInner`] may as well just be shared
+    /// references, so it's `Sync` whenever the inner bits are.
+    unsafe impl<'a, T> Sync for LoanInner<'a, T>
+    where
+        T: Sync + ?Sized,
+        Event: Sync,
+    {
     }
 }
 
+use inner::LoanInner;
+
+/// A simple one-shot event.
 struct Event(rsevents::ManualResetEvent);
 
 impl Event {
@@ -171,19 +338,19 @@ impl<'a, T: ?Sized> fmt::Pointer for Lender<'a, T> {
     }
 }
 
-impl<T: 'static + fmt::Display + ?Sized> fmt::Display for Loan<T> {
+impl<T: fmt::Display + ?Sized> fmt::Display for Loan<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(self.deref(), f)
     }
 }
 
-impl<T: 'static + fmt::Debug + ?Sized> fmt::Debug for Loan<T> {
+impl<T: fmt::Debug + ?Sized> fmt::Debug for Loan<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(self.deref(), f)
     }
 }
 
-impl<T: 'static + ?Sized> fmt::Pointer for Loan<T> {
+impl<T: ?Sized> fmt::Pointer for Loan<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Pointer::fmt(&(self.deref() as *const T), f)
     }
@@ -192,7 +359,7 @@ impl<T: 'static + ?Sized> fmt::Pointer for Loan<T> {
 impl<'a, T: PartialEq + ?Sized> PartialEq for LoanInner<'a, T> {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
-        self.principal == other.principal
+        self.principal() == other.principal()
     }
 }
 
@@ -201,20 +368,20 @@ impl<'a, T: Eq + ?Sized> Eq for LoanInner<'a, T> {}
 impl<'a, T: PartialOrd + ?Sized> PartialOrd for LoanInner<'a, T> {
     #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.principal.partial_cmp(other.principal)
+        self.principal().partial_cmp(other.principal())
     }
 }
 
 impl<'a, T: Ord + ?Sized> Ord for LoanInner<'a, T> {
     #[inline]
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.principal.cmp(other.principal)
+        self.principal().cmp(other.principal())
     }
 }
 
 impl<'a, T: hash::Hash + ?Sized> hash::Hash for LoanInner<'a, T> {
     #[inline]
     fn hash<H: hash::Hasher>(&self, state: &mut H) {
-        self.principal.hash(state);
+        self.principal().hash(state);
     }
 }
